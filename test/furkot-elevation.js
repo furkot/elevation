42cd25020d00d33b57f9dd5702974721f6033d80
Damian Krzeminski <pirxpilot@furkot.com>
2019-01-31 19:58:11 -0700

    add random service for testing

--- test/furkot-elevation.js
+++ test/furkot-elevation.js
@@ -1,9 +1,19 @@
-var should = require('should');
-var furkotElevation = require('../');
+const should = require('should');
+const furkotElevation = require('../');
 
-describe('furkot-elevation node module', function () {
-  it('must have at least one test', function () {
-    furkotElevation();
-    should.fail('Need to write tests.');
+describe('furkot elevation', function () {
+
+  it('uses random service by default', function (done) {
+    const points = [ [ 0, 0 ], [ 1, 1 ] ];
+    const elevation = furkotElevation({
+      random_enable: true
+    });
+    elevation(points, (err, result) => {
+      should.exist(result);
+      should.not.exist(err);
+      result.should.have.length(points.length);
+      done();
+    });
   });
+
 });
